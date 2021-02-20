71d13d91f82ab07b0482e33795a5e3349848f9ed
unknown <asemali2210@gmail.com>
2021-02-20 06:39:35 +0200

    asem

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import Protfolio from './pages/Protfolio';
 import Contact from './pages/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap/dist/react-bootstrap';
-import "animate.css/animate.min.css";
 import './style.scss'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from "react-router-dom"
 ReactDOM.render(
   <React.StrictMode>
       <Router>
+      <Switch>
           <Route exact path="/">
             <Home />
           </Route>
@@ -27,6 +28,7 @@ ReactDOM.render(
           <Route exact path="/contact">
             <Contact />
           </Route>
+      </Switch>
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
