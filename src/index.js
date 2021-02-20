efc5c56057e17d52737e2866cf01d1304d20ca15
unknown <asemali2210@gmail.com>
2021-02-20 07:48:17 +0200

    asem

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
-import Protfolio from './pages/Protfolio';
-import Contact from './pages/Contact';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap/dist/react-bootstrap';
-import './style.scss'
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom"
+import App from './pages/App'
+
 ReactDOM.render(
   <React.StrictMode>
-      <Router>
-          <Route exact path="/">
-            <Blog />
-          </Route>
-          <Route exact path="/blog">
-            <Blog />
-          </Route>
-          <Route exact path="/protfolio">
-            <Protfolio />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-      </Router>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/App.js
@@ -0,0 +1,35 @@
+import Home from './Home';
+import Blog from './Blog';
+import Protfolio from './Protfolio';
+import Contact from './Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap/dist/react-bootstrap';
+import '../style.scss'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+  } from "react-router-dom"
+function App(){
+    return(
+    <Router>
+        <Switch>
+
+        <Route exact path="/">
+            <Home />
+            </Route>
+            <Route exact path="/blog">
+            <Blog />
+            </Route>
+            <Route exact path="/protfolio">
+            <Protfolio />
+            </Route>
+            <Route exact path="/contact">
+            <Contact />
+            </Route>
+        </Switch>
+    </Router>
+    )
+}
+
+export default App;
